5908b7b012ed1d1098aba482c810254a5a0d17c5
Irsyad Rahman <irsyadrsi@gmail.com>
2017-04-21 17:47:31 +0700

    Initial Commit

---
+++ app/src/main/java/com/metamorfive/app/notiv/AProfile.java
@@ -0,0 +1,13 @@
+package com.metamorfive.app.notiv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_profile_view);
+    }
+}
